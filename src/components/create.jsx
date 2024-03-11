be815c95c19e8d5c8ffecf80ba8f20677b81bbeb
djo kangoma <djokangoma@gmail.com>
2024-03-11 21:40:11 +0100

    management of the form

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export default function Create() {
+  const [formData, setFormData] = useState({
+    inputName: "",
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: formData });
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    setFormData;
+  };
+
   return (
     <div>
       <h1>Add a User</h1>
-      <form action=""></form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="">Name</label>
+        <input
+          type="text"
+          placeholder="name"
+          name="inputName"
+          {...register("tweetInput", {
+            minLength: {
+              value: 1,
+              message: "le texte doit contenir au moins un caractere",
+            },
+          })}
+        />
+        {errors.tweetInput && (
+          <span style={{ color: "red" }}>{errors.tweetInput.message}</span>
+        )}
+      </form>
       <button>Submit</button>
       <button>Back</button>
     </div>
